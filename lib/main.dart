1d415b18c3c4728f8a2bd54cbe6632fa4b676739
kalouseckJP <juanpablo.rodriguez@alumnos.ulagos.cl>
2023-12-05 19:42:44 -0300

    arreglos - uso de card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -16,10 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pokedex'),
     );
   }
 }
@@ -47,77 +46,111 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Card(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 400.0,
-                  autoPlay: true
-                  ),
-                items: [0, 1, 2].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: const BoxDecoration(color: Color.fromARGB(255, 224, 224, 225)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 460.0,
+                autoPlay: true,
+                ),
+              items: [0, 1, 2].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Card(
                         child: Column(
                           children: <Widget>[
                             Image.asset(images[i]['image']!),
-                            Text(images[i]['nombre']!),
+                            const SizedBox(height: 20),
+                            Text(
+                              images[i]['nombre']!,
+                              style: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                            ),
+                            const SizedBox(height: 20),
                             SizedBox(
                               width: 300,
                               child: Row(
                                 children: <Widget>[
                                   if(images[i]['tipo']=='Electrico')
                                     Container(
+                                      width: 150,
                                       padding: const EdgeInsets.only(
                                         left: 10,
                                         right: 10,
                                       ),
                                       decoration: const BoxDecoration(
-                                        color: Colors.amber
+                                        color: Colors.amber,
+                                        borderRadius: BorderRadius.all(Radius.circular(22)),
                                       ),
-                                      child: Text(images[i]['tipo']!),
+                                      child: Text(images[i]['tipo']!,style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
                                   if(images[i]['tipo']=='Fuego')
                                     Container(
+                                      width: 150,
                                       padding: const EdgeInsets.only(
                                         left: 10,
                                         right: 10,
                                       ),
                                       decoration: const BoxDecoration(
-                                        color: Colors.red
+                                        color: Colors.red,
+                                        borderRadius: BorderRadius.all(Radius.circular(22)),
                                       ),
-                                      child: Text(images[i]['tipo']!),
+                                      child: Text(images[i]['tipo']!,style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
                                   if(images[i]['tipo']=='Agua')
                                     Container(
+                                      width: 150,
                                       padding: const EdgeInsets.only(
                                         left: 10,
                                         right: 10,
                                       ),
                                       decoration: const BoxDecoration(
-                                        color: Colors.cyan
+                                        color: Colors.cyan,
+                                        borderRadius: BorderRadius.all(Radius.circular(22)),
                                       ),
-                                      child: Text(images[i]['tipo']!),
+                                      child: Text(images[i]['tipo']!,style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
-                                const SizedBox(width: 100,),
-                                Text('Numero: ' + images[i]['numero']!)
+                                  Container(
+                                    width: 150,
+                                    child: Text('N.° ${images[i]['numero']!}', style: const TextStyle(
+                                        fontSize: 22,
+                                      ),
+                                      textAlign: TextAlign.right,
+                                    ),
+                                  ),
                                 ],
                               ),
                             )
                           ],
-                        )
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
+                        ),
+                      )
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ],
         ),
       ),
     );
